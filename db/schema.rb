8cb5e9ef89c39c94cccffa8fae1e414c9e7aa94a
Jonathan Marchand <jon.marchand1@gmail.com>
2016-10-03 14:48:49 -0400

    create view file for articles

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20161003183851) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
