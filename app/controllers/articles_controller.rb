11a3b263d19fee466d5c70f2ff0da31df44d19c3
Jonathan Marchand <jon.marchand1@gmail.com>
2016-10-03 21:32:55 -0400

    working on editing articles under a category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,40 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @category = Category.find_by(id: params[:category_id])
-    @article = Article.where(id: params[:ids])
+    @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    category = Category.find(params[:category_id])
+
+    @article.category = category
+    @article.save
+
+    redirect_to category_articles_url(category)
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      patch category_articles_url(category)
+    else
+      render 'edit'
+    end
+  end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
